56215ac617a9fdb43ed48198eb83df97d5e688cf
Aster <galaster@foxmail.com>
2022-09-28 23:46:36 +0800

    Add unicode property bool

---
+++ tests/generate.sh
@@ -0,0 +1,4 @@
+ucd-generate property-bool tests/UCD --chars > src/builtin/property_bool.rs
+ucd-generate general-category tests/UCD > src/builtin/ general_category.rs
+ucd-generate property-values tests/UCD > src/builtin/property_values.rs
+ucd-generate regex tests/UCD --chars > src/builtin/regex.rs
